fbd1ed45cfe26097ef69907075ba3ba238adc141
Wassim El Fen <wassimelfen@gmail.com>
2021-10-09 23:53:45 +0100

    save and reset buttons

--- Tasks.js
+++ Tasks.js
@@ -1,16 +1,28 @@
 function renderTask() {
+  $("#tasks-container").show();
   var input = $("#idInput").val();
+  var allTasks = [];
+  allTasks.push(input);
   $("#tasks-container").append(
     `<div id="task-shown">${input}<button class="remove" onclick="removeTask()" type="button">remove</button></div>`
   );
 }
 
 function removeTask() {
-  $("#task-shown").addClass("rrr");
-  $(".rrr").remove();
+  $("#task-shown").remove();
 }
 
-function day0() {
-  $(".days").append(`<h1>Your Todo list for monday</h1>`);
+localStorage.setItem("monday", "monday");
+var t = localStorage.getItem("monday");
+$(".days").append(`<h1>Your Todo list for </h1>` + `<h1>` + t + `</h1>`);
+
+function reset() {
+  $("#tasks-container").hide();
+  $("#task-shown").remove();
 }
-//// local Storage
+
+function save() {
+  $("button").hide();
+  $("input").hide();
+  $("body").append(`<h1>Challenge yourself to do all the tasks</h1>`);
+}
