264352fa6b5563752a923c496b641cea26ccfc40
Wassim El Fen <wassimelfen@gmail.com>
2021-10-10 12:26:10 +0100

    design improvment

--- Tasks.js
+++ Tasks.js
@@ -14,7 +14,9 @@ function removeTask() {
 
 localStorage.setItem("monday", "monday");
 var t = localStorage.getItem("monday");
-$(".days").append(`<h1>Your Todo list for </h1>` + `<h1>` + t + `</h1>`);
+$(".days").append(
+  `<h1 class="txt">Your Todo list for </h1>` + `<h1 class="txt1">` + t + `</h1>`
+);
 
 function reset() {
   $("#tasks-container").hide();
@@ -24,5 +26,11 @@ function reset() {
 function save() {
   $("button").hide();
   $("input").hide();
-  $("body").append(`<h1>Challenge yourself to do all the tasks</h1>`);
+  $("body").append(
+    `<h1 class="txt">Challenge yourself to do all the tasks</h1>`
+  );
+  $("#tasks-container").css(
+    "background-image",
+    "url('https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.hunterdonhealthcare.org%2Fwp-content%2Fuploads%2F2019%2F05%2FBlue-Gradient-Background-1024x374.jpg&f=1&nofb=1')"
+  );
 }
